5d6b0b26219548aa1705fd56e98542ef59124d56
Dolu Obatusin <mobatusin.tcsec@gmail.com>
2018-11-04 13:14:43 -0500

    add link to documentation tab, and complete plot and table in first tab

--- ShinyAppTutorials/course_project/app.R
+++ ShinyAppTutorials/course_project/app.R
@@ -19,34 +19,34 @@
 
 
 ###
-
-library(shiny)
-library(ggplot2)
-library(DT)
+library(shiny) # should always be activated
+library(ggplot2) # for the diamonds dataset, and ggplot feature
+library(DT) # for the dataTableOutput
 library(shinythemes)
+
 
 ui <- navbarPage(strong("The Mining Stock Scale"),theme = shinytheme("yeti"),
            tabPanel("Adjust your Mining stocks",
              wellPanel(
-                    sliderInput(inputId = "G1slider",
+                    sliderInput(inputId = "w1",
                                 label = "Weight on Grade 1",
                                 value = 1, min = 0 , max = 20),
-                    sliderInput(inputId = "G2slider",
+                    sliderInput(inputId = "w2",
                                 label = "Weight on Grade 2",
                                 value = 1, min = 0 , max = 20),
-                    sliderInput(inputId = "G3slider",
+                    sliderInput(inputId = "w3",
                                 label = "Weight on Grade 3",
                                 value = 1, min = 0 , max = 6)
                     ),
                     plotOutput("plot", brush = "user_brush"),
-                    dataTableOutput("table"),
+                    DT::dataTableOutput("table"),
                     downloadButton(outputId = "downlaod", label="Download Table")
              ),
            
            tabPanel("Documentation",
-                    tabPanel("Video guide to Shiny App, Iframe",
-                             tags$iframe(style="height:600px; width:100%; scrolling=yes", 
-                                         src="https://youtu.be/Gyrfsrd4zK0"))),
+                    h4("Video guide to Shiny App, Iframe",
+                             tags$iframe(style="height:700px; width:100%; scrolling=yes", 
+                                         src="https://www.youtube.com/embed/vySGuusQI3Y"))),
            
            tabPanel("Data Table With the Underlying Data", 
                     DT::dataTableOutput("tableOrig"))
@@ -54,51 +54,51 @@ ui <- navbarPage(strong("The Mining Stock Scale"),theme = shinytheme("yeti"),
           
 )
 
-server <- function(input, output, session) {
-  library(ggplot2) # for the diamonds dataset, and ggplot feature
-  library(DT) # for the dataTableOutput
-  library(shiny) # should always be activated
-  library(dplyr)
-  library(readr)
-  
-  #values <- reactiveValues()
-  
-  data <- read.csv("course-proj-data.csv", sep=";")
-  
-  datascore <- reactive({ (data['G1']*input$G1slider) + (data['G2']*input$G2slider) + (datascore['G3']*input$G3slider)
-  })
-  
-  #datascore <- reactive(values$data['score'] <- score )
+data <- read.csv("course-proj-data.csv", sep=";")
+attach(data)
 
-  output$plot <- renderPlot({
-    ggplot(data, aes(G2,MarketCap.in.M) ) + stat_summary(fun.data=mean_cl_normal) + 
-      geom_smooth(method='lm',formula=y~x) + geom_point()
-  })
-  
-  diam <- reactive({
+server <- function(input, output, session) {
+
+
+  weighted.data = reactive(
+    cbind(data, 
+          points = `G1`*input$w1 + `G2`*input$w2 + `G3`*input$w3)
+      )
     
+  # Plot of calculated score and market capitalization in millions USD
+  output$plot <- renderPlot({
+      ggplot(weighted.data(), aes(points,MarketCap.in.M) ) + geom_point() + geom_smooth(method='lm')  +
+        xlab("Your calculated score") + ylab("Market Capitalization in Million USD")
+    })
+    
+  # Create new reactive data with brushed points
+  data.new = reactive({
+
     user_brush <- input$user_brush
-    sel <- brushedPoints(data, user_brush)
+    sel <- brushedPoints(weighted.data(), user_brush)
     return(sel)
     
   })
   
-  output$table <- DT::renderDataTable({datatable(diam())
-    })
+  # Data for the Adjusting your mining stocks tab
+  output$table <- DT::renderDataTable(DT::datatable(data.new()))
   
-  output$tableOrig <- DT::renderDataTable({datatable(data , options = list(paging=F), rownames = F, 
-                                      filter='top') %>%
-                                        formatCurrency("MarketCap.in.M", "$") %>%
-                                        formatStyle("G1", backgroundColor = "lightblue")  %>%
-                                        formatStyle("G2", backgroundColor = "lightblue")  %>%
-                                        formatStyle("G3", backgroundColor = "lightblue")
-                                      })
-  
+  # Download tab
   output$download <- downloadHandler(
-    filename = "project_downlaod.csv",
+    filename = "selected_miners.csv",
     content = function(file) {
-      write.csv(diam(), file)
+      write.csv(data.new(), file)
     }
+  )
+  
+  # Data for the data table tab
+  output$tableOrig <- DT::renderDataTable(DT::datatable(data , options = list(paging=F), rownames = F, 
+                                                     filter='top') %>%
+      formatCurrency("MarketCap.in.M", "$", digits = 0) %>%
+      formatStyle("Symbol", color = "grey")  %>%
+      formatStyle("G1", backgroundColor = "lightblue")  %>%
+      formatStyle("G2", backgroundColor = "lightblue")  %>%
+      formatStyle("G3", backgroundColor = "lightblue")
   )
   
 }
